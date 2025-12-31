5ff765435037b67d8cc43b1da4ccd44a90074fa0
lxxjune <petdoc.yoon@gmail.com>
2025-12-31 21:39:28 +0900

    Fix: Update GA tracking ID to valid one

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-B5QLLD5914';
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-D69WY25XPH';
 
 export const pageview = (url: string) => {
   if (typeof window !== 'undefined' && (window as any).gtag) {
